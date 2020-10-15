10ebc7023635e1098cddfe32f18cdeeff8573210
ricardolp <ricardo.lopez@interware.com.mx>
2020-10-15 16:57:41 -0500

    add web integration test

--- src/test/java/mx/gob/nayatit/ProducerGenericApplicationTests.java
+++ src/test/java/mx/gob/nayatit/ProducerGenericApplicationTests.java
@@ -1,13 +1,40 @@
 package mx.gob.nayatit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mx.gob.nayatit.controller.ProducerController;
+import mx.gob.nayatit.model.Producer;
+import mx.gob.nayatit.service.ProducerService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest(ProducerController.class)
 class ProducerGenericApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@MockBean
+	private ProducerService<Producer> producerService;
+
+	@Test
+	void contextLoads() throws Exception {
+		Producer producer = new Producer();
+		producer.setName("ric");
+		producer.setQuota(1);
+		Mockito.when(producerService.save(producer)).thenReturn("id:1");
+		this.mockMvc.perform(MockMvcRequestBuilders.
+				post("/producer")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(new ObjectMapper().writeValueAsString(producer))
+				).
+				andExpect(MockMvcResultMatchers.status().isCreated());
+
+	}
 }
